96858ec002b9b2f510329ac005896023777fdbc8
Eleanarb <leangieriera@gmail.com>
2022-11-29 23:29:08 -0300

    encriptar url de cambiar clave

--- RecuperarClave.aspx.cs
+++ RecuperarClave.aspx.cs
@@ -9,6 +9,8 @@ using Dao;
 using System.Net.Mail;
 using System.Net;
 using System.Text;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace ManagCond
 {
@@ -46,7 +48,8 @@ namespace ManagCond
 
             if (resul > 0)
             {
-                Notificacion(correo, usuario.Nombres);
+                string emailDestino = HttpUtility.UrlEncode(Encrypt(correo));
+                Notificacion(emailDestino, usuario.Nombres);
                 Session["mensaje"] = "1";
                 Response.Redirect("RecuperarClave.aspx");
             }
@@ -57,6 +60,28 @@ namespace ManagCond
             }
 
 
+        }
+
+        private string Encrypt(string clearText)
+        {
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
         }
 
         public static string MakeExpiryHash(DateTime expiry)
