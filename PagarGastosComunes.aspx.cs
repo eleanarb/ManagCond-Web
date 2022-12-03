a645d9eee43272b1ebfe0aa1118db68b2ec50fcc
Eleanarb <leangieriera@gmail.com>
2022-12-03 17:52:29 -0300

    no se

--- PagarGastosComunes.aspx.cs
+++ PagarGastosComunes.aspx.cs
@@ -34,7 +34,7 @@ namespace ManagCond
                     expiresDate: dt,
                     body: "Descripción de la compra",
                     pictureUrl: "http://mi-ecomerce.com/pictures/foto-producto.jpg",
-                    returnUrl: "http://mi-ecomerce.com/backend/return",
+                    returnUrl: "https://managcond.azurewebsites.net/PagoRealizado.aspx/",
                     cancelUrl: "http://mi-ecomerce.com/backend/cancel",
                     notifyUrl: "http://mi-ecomerce.com/backend/notify",
                     notifyApiVersion: "1.3"
--- PagoRealizado.aspx.cs
+++ PagoRealizado.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Khipu.Api;
+using Khipu.Client;
+using Khipu.Model;
 
 namespace ManagCond
 {
@@ -11,7 +14,34 @@ namespace ManagCond
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Configuration.ReceiverId = 432730;
+            Configuration.Secret = "21e14d913cd4ee4bd3176fb1808130c6b7c23ca9";
+            string notificationToken = "obtener-desde-los-parametros-del-request";
+            string apiVersion = "obtener-desde-los-parametros-del-request";
+            double amount = 100;
 
+            if (apiVersion.Equals("1.3"))
+            {
+                PaymentsApi a = new PaymentsApi();
+                try
+                {
+                    PaymentsResponse response = a.PaymentsGet(notificationToken);
+                    if (response.ReceiverId.Equals(Configuration.ReceiverId)
+                           && response.Status.Equals("done") && response.Amount == amount)
+                    {
+                        
+                    }
+                    else
+                    {
+                    }
+                    Page.Response.Write("<script>console.log('"+response.NotificationToken+"');</script>");
+                    TextBoxNombre.Value = response.NotificationToken;
+                }
+                catch (ApiException ev)
+                {
+                    Console.WriteLine(ev);
+                }
+            }
         }
     }
 }
